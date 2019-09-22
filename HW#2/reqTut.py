ce5daeaaf9331420b447458d284ff12b70831b8e
Shujaat Bakhsh <shujaatbakhsh25@gmail.com>
2019-09-21 20:08:38 -0400

    Added HW#2

---
+++ HW#2/reqTut.py
@@ -0,0 +1,35 @@
+import requests
+from html.parser import HTMLParser
+
+his = []
+hers = []
+
+
+class MyHTMLParser(HTMLParser):
+    def __init__(self):
+        super().__init__()
+        self.currentTag = None
+
+    def handle_starttag(self, tag, attrs):
+        if tag == "p":
+            self.currentTag = tag
+        else:
+            pass
+
+    def handle_data(self, data):
+        if self.currentTag == "p" and data.startswith('He'):
+            his.append(data)
+            # hers.append(data.split('.')[1])
+
+    def clean(self):
+        self.reset()
+
+
+myParser = MyHTMLParser()
+for i in range(10):
+    r = requests.get("https://theyfightcrime.org")
+    myParser.feed(r.text)
+    myParser.clean()
+
+for i in his:
+    print(i)
